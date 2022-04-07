a01e8e592bb06dab5f1edec1c306112ab3164b57
Sergey Onischuk <sergey.onischuk@ukr.net>
2022-04-07 21:08:23 +0300

    successful decision

--- HelloJava.java
+++ HelloJava.java
@@ -1,16 +1,20 @@
 public class HelloJava {
 
     public static void main(String[] args) {
-        System.out.println(getFirstDigit(15));
-        System.out.println(getSecondDigit(15));
+        System.out.println(getSecondDigit(789));
+        System.out.println(getSecondDigit(12));
+        System.out.println(getSecondDigit(4563));
+        System.out.println(getFirstDigit(12345));
+        System.out.println(getFirstDigit(67));
+        System.out.println(getFirstDigit(890));
     }
 
         public static int getFirstDigit(int number) {
             String numberString = Integer.toString(number);
-            return Integer.parseInt(numberString.substring(0,1));
+            return Integer.parseInt(numberString.substring(0, numberString.length()-1));
         }
         public static int getSecondDigit(int number) {
             String numberString = Integer.toString(number);
-            return Integer.parseInt(numberString.substring(1,2));
+            return Integer.parseInt(numberString.substring(numberString.length()-1));
         }
 }
